bd266634b100209ae11ebc0b04ff1cc30824e1ae
nithinmanoj10 <nithinp.manoj@gmail.com>
2021-12-21 13:57:23 +0400

    :pencil2: Stage 0 : LEX Exercise 1.1, 1.2 & 1.3 completed

--- Lex Tutorial/Exercises/lex.yy.c
+++ Lex Tutorial/Exercises/lex.yy.c
@@ -363,9 +363,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[10] =
     {   0,
-        0,    0,    4,    3,    2,    1,    1,    0
+        2,    2,    4,    2,    3,    1,    2,    1,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -376,7 +376,7 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    3,    3,    3,
         3,    3,    3,    3,    3,    3,    3,    1,    1,    1,
-        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
+        1,    1,    1,    1,    4,    3,    3,    3,    3,    3,
         3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
         3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
         1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
@@ -400,29 +400,33 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[4] =
+static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    1,    2
+        1,    2,    1,    1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[12] =
     {   0,
-        0,    0,    5,    6,    6,    0,    0,    6,    2
+        0,    0,    9,    0,   10,    7,    0,    6,   10,    5,
+        4
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[12] =
     {   0,
-        8,    1,    8,    8,    8,    9,    9,    0,    8
+        9,    1,    9,   10,    9,   11,   10,   11,    0,    9,
+        9
     } ;
 
-static const flex_int16_t yy_nxt[10] =
+static const flex_int16_t yy_nxt[15] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    4,    6,    8,    7,    7,    7,    9,    3,
+        9,    9,    9,    9
     } ;
 
-static const flex_int16_t yy_chk[10] =
+static const flex_int16_t yy_chk[15] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    1,   11,   10,    8,    6,    3,    9,
+        9,    9,    9,    9
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -439,14 +443,17 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "yyex_1.l"
-#line 4 "yyex_1.l"
+#line 1 "yyex_3.l"
+#line 2 "yyex_3.l"
+	// Lex program to list out all words of length three, starting with "A" to uppercase
+
 	#include <stdio.h>
 	#include <stdlib.h>
+	#include <wchar.h>
+	#include <locale.h>
 
-	int lineCount = 0, wordCount = 0, charCount = 0;
-#line 449 "lex.yy.c"
-#line 450 "lex.yy.c"
+#line 456 "lex.yy.c"
+#line 457 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -663,10 +670,10 @@ YY_DECL
 		}
 
 	{
-#line 13 "yyex_1.l"
+#line 15 "yyex_3.l"
 
 
-#line 670 "lex.yy.c"
+#line 677 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -693,13 +700,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 10 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 10 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -725,21 +732,33 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 15 "yyex_1.l"
-{++wordCount; charCount += yyleng;}
+#line 17 "yyex_3.l"
+{
+	setlocale(LC_ALL, "en_US.utf8");
+	const wchar_t correctEmoji = 0x2705;
+	const wchar_t wrongEmoji = 0x274c;
+
+	if(yyleng == 3)
+		printf("%lc %s", correctEmoji,yytext);
+	else
+		printf("%lc %s", wrongEmoji,yytext);
+} 
 	YY_BREAK
 case 2:
-/* rule 2 can match eol */
 YY_RULE_SETUP
-#line 16 "yyex_1.l"
-{++lineCount;}
+#line 28 "yyex_3.l"
+{
+	setlocale(LC_ALL, "en_US.utf8");
+	const wchar_t wrongEmoji = 0x274c;
+	printf("%lc %s", wrongEmoji,yytext);
+} 
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 18 "yyex_1.l"
+#line 34 "yyex_3.l"
 ECHO;
 	YY_BREAK
-#line 743 "lex.yy.c"
+#line 762 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1036,7 +1055,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 10 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1064,11 +1083,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 10 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 9);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1744,20 +1763,26 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 18 "yyex_1.l"
+#line 34 "yyex_3.l"
 
 
-int main(int argc, char* argv[]){
-	if(argc > 1){
-		FILE* fp = fopen("input_1.txt", "r");
-		if(fp)
-			yyin = fp;
-	}
-	
+int main(){
+
+	setlocale(LC_ALL, "en_US.utf8");
+	const wchar_t codeEmoji = 0x1F4D1;
+	const wchar_t stageEmoji = 0x1F4C0;
+	const wchar_t inputEmoji = 0x1F4E5;
+	const wchar_t resultEmoji = 0x2728;
+	const wchar_t diamondEmoji = 0x1F539;
+
+	printf("%lc Stage 0 : Lex Tutorial\n", stageEmoji);
+        printf("\n%lc Lex program to list out all words of length three, starting with 'A' to uppercase\n\n", codeEmoji);
+
+	FILE* fp = fopen("input_3.txt", "r");
+	if(fp)
+		yyin = fp;
+
 	yylex();
-	printf("\nNumber of Lines: %d\n", lineCount);
-	printf("Number of Words: %d\n", wordCount);
-	printf("Number of Characters: %d\n", charCount);
 	return 1;
 }
 
