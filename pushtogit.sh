a3cad052d20d46317def4869d07fb675b8d6834c
nithinmanoj10 <nithinp.manoj@gmail.com>
2022-01-13 10:07:08 +0400

    🔨 Change to pushtogit script

--- pushtogit.sh
+++ pushtogit.sh
@@ -8,14 +8,20 @@ echo "📁 Pushing Files ➡ nithinmanoj10/Compiler-Lab-NITC ...\n"
 # Adding all the files
 git add -A
 
+printf '%.s─' $(seq 1 $(tput cols))
 git status
+printf '%.s─' $(seq 1 $(tput cols))
 
 printf "\n📄 Enter Commit Message: "
 read commitMessage
+printf "\n"
 
+printf '%.s─' $(seq 1 $(tput cols))
 git commit -m "$commitMessage"
+printf '%.s─' $(seq 1 $(tput cols))
 
-git push origin master
+# git push origin master
 
-printf "nithinmanoj10\n"
-printf "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\n"
+printf '%.s─' $(seq 1 $(tput cols))
+printf "nithinmanoj10\n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\n" | git push origin master
+printf '%.s─' $(seq 1 $(tput cols))
